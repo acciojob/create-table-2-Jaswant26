92c3b3039c8fd6c9c9d99fe57149d67e7131337d
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-11-05 20:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function createTable() {
     //Write your code here
-  
+   const rowNum = prompt("Input number of rows");
+  const colNum = prompt("Input number of columns");
+
+  // Convert the input values to integers
+  const numRows = parseInt(rowNum);
+  const numCols = parseInt(colNum);
+
+  if (!isNaN(numRows) && !isNaN(numCols)) {
+    const table = document.getElementById("myTable");
+    table.innerHTML = ""; // Clear any existing content in the table
+
+    // Create the table rows and cells
+    for (let i = 0; i < numRows; i++) {
+      const row = table.insertRow(i);
+      for (let j = 0; j < numCols; j++) {
+        const cell = row.insertCell(j);
+        cell.textContent = `Row-${i} Column-${j}`;
+      }
+    }
+  } else {
+    alert("Please enter valid numbers for rows and columns.");
+  }
 }
